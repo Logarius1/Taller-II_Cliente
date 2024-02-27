713b9c0fd18aebc8ca63a6a379d4803f02f6f384
Cande0503 <134917662+Cande0503@users.noreply.github.com>
2024-02-27 19:41:38 -0300

    Agregue Inscripcion Asignatura a la capa grafica

---
+++ CapaGrafica/grafica/InscripcionAlumno.java
@@ -0,0 +1,35 @@
+package grafica;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+
+public class InscripcionAlumno extends JInternalFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InscripcionAlumno frame = new InscripcionAlumno();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InscripcionAlumno() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
